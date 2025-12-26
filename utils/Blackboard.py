d33c9b9cb4bf43d057d35cd721f1f733e7a76a6e
TheDarKnight50 <aryankay1234@gmail.com>
2025-12-26 13:10:46 +0530

    Blackboard final stucture

--- utils/Blackboard.py
+++ utils/Blackboard.py
@@ -1,23 +1,36 @@
 from collections import deque
 
-from utils.utils import load_property_config
-PROPERTY_CONFIG = load_property_config("configs/PropertyConfig.yaml")
+from utils import load_property_config
+PROPERTY_CONFIG = load_property_config("configs/final_config.yaml")
 
 class Blackboard:
     """The Shared Memory."""
-    def __init__(self):
-        # Dynamic queues: {'needs_fix:toxicity': [], 'needs_fix:solubility': []}
-        self.task_queues = {f"needs_fix:{k}": deque() for k in PROPERTY_CONFIG.keys()}
+    def __init__(self, config):
+        all_properties = []
+        
+        if "potency" in config:
+            all_properties.append("potency")
+
+        if "hard_filters" in config:
+            all_properties.extend(config["hard_filters"].keys())
+
+        if "soft_filters" in config:
+            all_properties.extend(config["soft_filters"].keys())
+
+        self.task_queues = {f"needs_fix:{k}": deque() for k in all_properties}
         self.hall_of_fame = []
 
     def post_task(self, property_to_fix, z, current_scores):
         tag = f"needs_fix:{property_to_fix}"
         if tag in self.task_queues:
             self.task_queues[tag].append((z, current_scores))
+        else:
+            print(f"Warning: No queue found for {property_to_fix}")
 
-    def fetch_task(self, agent_id):
-        """Fetch a task for a specific agent."""
-        for key, queue in self.task_queues.items():
-            if queue:
-                return key.split(":")[1], *queue.popleft()
+    def fetch_task(self, property_to_fix):
+        tag = f"needs_fix:{property_to_fix}"
+        if tag in self.task_queues and self.task_queues[tag]:
+            return self.task_queues[tag].popleft()
+        else:
+            print(f"Warning: No task found for {property_to_fix}. Need to turn back to exploration.")
         return None
