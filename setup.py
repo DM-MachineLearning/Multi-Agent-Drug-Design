bce4ee8ea2a6ed4a825dbc6d753fc9ce1b977536
Aryan <aryankay1234@gmail.com>
2026-04-13 11:01:50 +0000

    Removed unnecessary files from main directory.

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_namespace_packages # Change this line
-
-setup(
-    name="multi_agent_drug_design",
-    version="0.1",
-    packages=find_namespace_packages(include=["Agents*", "Generators*", "Datasets*", "src*", "utils*", "Models*"]),
-)
